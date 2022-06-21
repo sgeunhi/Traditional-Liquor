f43b48bd2c244278faa6f52f2fb2fd7a833076eb
sgeunhi <alsrb582@snu.ac.kr>
2022-06-21 15:40:33 +0900

    Rate model 수정

--- src/Entity/Rate.js
+++ src/Entity/Rate.js
@@ -1,7 +1,8 @@
 export class Rate {
-    constructor(id, userId, alcoholId, numberOfStars, reviewText, timestamp) {
+    constructor(id, userId, userNickname, alcoholId, numberOfStars, reviewText, timestamp) {
         this.id = id;
         this.userId = userId;
+        this.userNickname = userNickname
         this.alcoholId = alcoholId;
         this.numberOfStars = numberOfStars;
         this.reviewText = reviewText;
@@ -15,6 +16,7 @@ export class Rate {
     toData() {
         return {
             userId: this.userId,
+            userNickname: this.userNickname,
             alcoholId: this.alcoholId,
             numberOfStars: this.numberOfStars,
             reviewText: this.reviewText,
